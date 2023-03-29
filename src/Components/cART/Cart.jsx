49d98891b7efdac200678e24a106ede3688a4168
Shahinur04 <shahinur04042002@gmail.com>
2023-03-30 05:44:44 +0600

    complete oder

--- src/Components/cART/Cart.jsx
+++ src/Components/cART/Cart.jsx
@@ -8,15 +8,21 @@ const Cart = (props) => {
   for (let product of cart) {
     total = total + product.price;
   }
+  let shipping = 0;
+  for (let product of cart) {
+    shipping = shipping + product.shipping;
+  }
+  let tax=total*7/100;
+  const grandTotal=shipping+total+tax;
   // console.log(cart)
   return (
     <div className="cart">
       <h2>Order summary</h2>
       <p>Selected items:{cart.length}</p>
       <p>Total Price:${total}</p>
-      <p>Total Shipping Charge:{}</p>
-      <p>Total Tax:{}</p>
-      <h6>Grand Total:{} </h6>
+      <p>Total Shipping Charge:${shipping}</p>
+      <p>Total Tax:${tax.toFixed(2)}</p>
+      <h2>Grand Total:${grandTotal.toFixed(2)} </h2>
       <button>Clear Cart</button>
       <button>Review Order</button>
     </div>
