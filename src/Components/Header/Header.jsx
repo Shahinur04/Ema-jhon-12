06021a682f3383aadd85c4c24280d858bffcc11e
HmSani6699 <shahinur04042002@gmail.com>
2023-04-08 00:25:45 +0600

    added route

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
         <nav className='header'>
            <img src={logo} alt="" />
            <div>
-           <a href="Shop">Shop</a>
+           <a href="/">Shop</a>
+           <a href="Orders">Order</a>
            <a href="Inventory">Inventory</a>
-           <a href="Order">Order</a>
            <a href="Login">Login</a>
            </div>
         </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Shop from "./Components/Shop/Shop";
+import Home from "./Components/Layout/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home></Home>,
+    children:[
+      {
+        path:'/',
+        element:<Shop></Shop>
+      }
+    ]
+  },
+  
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
